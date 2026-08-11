31842909d221ca785b6521557a665a7c60852a8c
AniketDobhada1 <AniketDobhada1@gmail.com>
2026-08-11 15:08:15 +0530

    feat(object-store): expose s3_delete function for file management

--- crates/kabipay-employee/src/services/object_store/mod.rs
+++ crates/kabipay-employee/src/services/object_store/mod.rs
@@ -9,6 +9,7 @@ mod s3_tenant;
 pub use config::FileStorageMode;
 pub use config::S3CompatSettings;
 pub use s3_tenant::ensure_tenant_bucket;
+pub use s3_tenant::s3_delete;
 pub use s3_tenant::s3_operator_for_bucket;
 pub use s3_tenant::s3_put;
 pub use s3_tenant::s3_read;
